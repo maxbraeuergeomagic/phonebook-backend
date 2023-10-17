0516e7df3d5f25f6a436debdfe83ea0de3cc0f7a
Max Bräuer <max.braeuer@geomagic.world>
2023-10-17 10:37:12 +0200

    uibuild

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 const cors = require("cors");
-
+app.use(express.static("build"));
 app.use(cors());
 
 let persons = [
